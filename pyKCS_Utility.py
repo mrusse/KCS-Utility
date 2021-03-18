7c723be225253bce62db358c5322c41171f5b753
meusse09 <99michaelrussell99@gmail.com>
2021-03-18 19:53:33 -0400

    added better settings (baud,select which setting)) and better file handling

--- pyKCS_Utility.py
+++ pyKCS_Utility.py
@@ -20,18 +20,27 @@ def encode_file():
     print("\nEncode file")
     infile = input("Input filename:")
 
-    print(infile)
-
     while not os.path.isfile(infile):
         print("File not found. Make sure the file is in the currect directory.")
         infile = input("Input filename:")
     
+    if os.path.isfile("toEncode.mjr"):
+        os.remove("toEncode.mjr")
+
     os.rename(infile,"toEncode.mjr")
     
     outfile = input("Output WAV filename:")
 
-    KCS = "KCS -M -Y -U -L5 " + "toEncode.mjr" + " " + "encode.wav"
+    if baud == "300":
+        KCS = "KCS -M -Y -L5 " + "toEncode.mjr" + " " + "encode.wav"
+    if baud == "1200":
+        KCS = "KCS -M -Y -U -L5 " + "toEncode.mjr" + " " + "encode.wav"
+
     dosbox_args = [r'mount c ' + cwd,'C:',KCS,'exit']
+
+    if os.path.isfile(outfile):
+        os.remove(outfile)
+
     run_dosbox(dosbox_args)
 
     os.rename("toEncode.mjr",infile)
@@ -48,20 +57,29 @@ def decode_file(infile):
         print("File not found. Make sure the file is in the currect directory.")
         infile = input("Input WAV filename:")
     
+    if os.path.isfile("toDecode.wav"):
+        os.remove("toDecode.wav")
+
     os.rename(infile,"toDecode.wav")
     
     outfile = input("Output filename:")
 
-    KCS = "KCS -Y -U " + "toDecode.wav" + " " + "decode.mjr"
+    if baud == "300":
+        KCS = "KCS -Y " + "toDecode.wav" + " " + "decode.mjr"
+    if baud == "1200":
+        KCS = "KCS -Y -U " + "toDecode.wav" + " " + "decode.mjr"
 
     dosbox_args = [r'mount c ' + cwd,'C:',KCS,'exit']
+
+    if os.path.isfile(outfile):
+        os.remove(outfile)
 
     run_dosbox(dosbox_args)
 
     os.rename("toDecode.wav",infile)
     os.rename("decode.mjr",outfile)
 
-    if infile == "output.wav":
+    if infile == "output.wav" and baud == "1200":
         f = open(outfile, 'rb')
         f.seek(1) # skip the first 1 byte
         trim = f.read()
@@ -195,14 +213,14 @@ def record_wav():
         data = stream.read(chunk)
         rms_data = rms(data)
         decibel = 20 * math.log10(rms_data)
-        if decibel < -10:
+        if decibel < -7:
             print("Listening for data... %.2f decibels press esc to abort " % (decibel),end = '\r')
-        if decibel > -10:
+        if decibel > -7:
             print("Recording data... %.2f decibels press esc to abort     " % (decibel),end = '\r')
             #data2 = stream.read(chunk)
             recorded = 1
             frames.append(data)
-        if recorded == 1 and decibel < -10:
+        if recorded == 1 and decibel < -7:
             break
         if keyboard.is_pressed('esc'):
             break
@@ -234,10 +252,77 @@ def record_wav():
         decode_file("output.wav")
         
 
-#initize dosbox location and devices
-def init_dos():
-    #get dosbox location (TODO check its a good filepath)
-    if not os.path.isfile("pyKCSconfig.txt"):
+#initize dosbox location and devices and baud
+def init_dos(from_settings):
+
+    if from_settings == True:
+        print("\nSETTINGS\n1.Edit DOSBox location\n2.Change default recording device\n3.Select baud rate\n")
+
+        setting_option = input("Select option:")
+        
+        while not setting_option != '1' and setting_option != '2' and setting_option != '3':
+            print("\nNot an option please select 1-3\n")
+            setting_option = input("Select option:")
+
+        with open("pyKCSconfig.txt", 'r') as settings:
+            lines = settings.readlines()
+
+        if setting_option == '1':
+
+            dosbox_location = input("Please input the filepath for DOSBox.exe\n"\
+                                "For example C:\\Program Files (x86)\\DOSBox-0.74-3\\DOSBox.exe\n"\
+                                "You only have to do this once:")
+            while not os.path.isfile(dosbox_location):
+                print("File not found.")
+                dosbox_location = input("Please input the filepath for DOSBox.exe\n"\
+                                "For example C:\\Program Files (x86)\\DOSBox-0.74-3\\DOSBox.exe\n"\
+                                "You only have to do this once:")
+
+            lines[0] = dosbox_location + "\n"
+            device_id = lines [1]
+            baud = lines[2]
+
+        if setting_option == '2':
+
+            p = pyaudio.PyAudio()
+            info = p.get_host_api_info_by_index(0)
+            numdevices = info.get('deviceCount')
+        
+            print("\nSelect input device to record from (this should be your cassette player)\n")
+        
+            rec_devices = 0
+        
+            for i in range(0, numdevices):
+                if (p.get_device_info_by_host_api_device_index(0, i).get('maxInputChannels')) > 0:
+                    print("Input Device id ", i, " - ", p.get_device_info_by_host_api_device_index(0, i).get('name'))
+                    rec_devices += 1
+                
+            device_id = int(input("Device ID:"))  
+        
+            while device_id > rec_devices or device_id < 0:
+                print("Cant find that ID")
+                device_id = int(input("Device ID:")) 
+
+            lines[1] = str(device_id) + "\n"
+            dosbox_location = lines[0]
+            baud = lines[2]
+
+        if setting_option == '3':
+
+            baud = input("\nWould you like to encode at 300 or 1200 baud? (300 is easier for lower quality cassette recorders):")
+
+            while baud != "300" and baud != "1200":
+                print("Invalid choice")
+                baud = input("\nWould you like to encode at 300 or 1200 baud? (300 is easier for lower quality cassette recorders):")
+
+            lines[2] = baud    
+            dosbox_location = lines[0]
+            device_id = lines[1]
+
+        with open("pyKCSconfig.txt", 'w') as settings:
+            settings.writelines( lines )
+
+    if not os.path.isfile("pyKCSconfig.txt") and from_settings == False:
         dosbox_location = input("Please input the filepath for DOSBox.exe\n"\
                                 "For example C:\\Program Files (x86)\\DOSBox-0.74-3\\DOSBox.exe\n"\
                                 "You only have to do this once:")
@@ -273,16 +358,26 @@ def init_dos():
             device_id = int(input("Device ID:"))
             
         location_file = open("pyKCSconfig.txt","a")
-        location_file.write(str(device_id))
+        location_file.write(str(device_id) + "\n")
+
+        baud = input("\nWould you like to encode at 300 or 1200 baud? (300 is easier for lower quality cassette recorders):")
+
+
+        while baud != "300" and baud != "1200":
+            print("Invalid choice")
+            baud = input("\nWould you like to encode at 300 or 1200 baud? (300 is easier for lower quality cassette recorders):")
+
+        location_file.write(baud)
         location_file.close()
-        
-    else:
+
+    elif from_settings == False:
         location_file = open("pyKCSconfig.txt","r")
         dosbox_location = location_file.readline().rstrip()
-        device_id = int(location_file.readline())
+        device_id = int(location_file.readline().rstrip())
+        baud = location_file.readline()
         location_file.close()
-        
-    return dosbox_location, device_id
+
+    return dosbox_location, device_id, baud
 
 
 #Calculate decibels
@@ -304,13 +399,13 @@ def menu():
 2.Decode file\n\
 3.Play WAV for cassette recording\n\
 4.Record cassette to WAV\n\
-5.Change settings (dosbox location, recording device)\n\
+5.Change settings (dosbox location, recording device, baud rate)\n\
 6.Exit\n")
 
     menu_option = input("Select option:")
 
-    while not menu_option == '1' and menu_option == '2' and menu_option == '3' and menu_option == '4' and menu_option == '5':
-        print("\nNot an option please sellect 1-4\n")
+    while menu_option != '1' and menu_option != '2' and menu_option != '3' and menu_option != '4' and menu_option != '5' and menu_option != '6':
+        print("\nNot an option please select 1-6\n")
         menu_option = input("Select option:")
 
     #Call correct function
@@ -333,14 +428,13 @@ def menu():
         record_wav()
         
     if menu_option == '5':
-        os.remove("pyKCSConfig.txt")
-        init_dos()
-        print ("\nRESTART the script for these changes to take effect.")
-        
+        dosbox_location, device_id, baud = init_dos(True)
+        print("%s%s%s" % (dosbox_location,device_id,baud))
+
     if menu_option == '6':
         quit()
 
-dosbox_location, device_id = init_dos()
+dosbox_location, device_id, baud = init_dos(False)
 #Working directory
 cwd = os.getcwd()
 
